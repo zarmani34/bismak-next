883277f1d2b99a77c633db0cabae3562f289ce94
[Abdulazeez Idowu] <[idowuabdulazeez39@gmail.com]>
2026-02-10 10:16:03 +0100

    Make portal sidebar responsive

--- src/app/portal/components/PortalSideBar.tsx
+++ src/app/portal/components/PortalSideBar.tsx
@@ -4,9 +4,12 @@ import Link from "next/link";
 import { ADMINPORTALMENU, CLIENTPORTALMENU, STAFFPORTALMENU } from "../constants";
 import { useState } from "react";
 import { usePathname } from "next/navigation";
+import Navbar from "@/src/components/Navbar";
+import Harmburger from "@/src/components/Hamburger";
 
 const PortalSidebar = () => {
   const [isCollapsed, setIsCollapsed] = useState(false);
+  const [showMenu, setShowMenu] = useState(false);
   const pathname = usePathname();
 
   type PortalType = "client" | "staff" | "admin";
@@ -34,13 +37,15 @@ const currentMenu = portalMenus[portalType];
   return (
     <>
       {/* Mobile Overlay */}
-      {/* {showMenu && (
+      {showMenu && (
         <div
-          className="md:hidden fixed inset-0 bg-gradient-to-r from-primary-light/10 to-primary-light/20 backdrop-blur-sm bg-opacity-50 z-20 transition-opacity duration-300"
-          onClick={closeMobileSidebar}
+          className="md:hidden fixed inset-0 bg-linear-to-r from-primary-light/10 to-primary-light/20 backdrop-blur-sm bg-opacity-50 z-20 transition-opacity duration-300"
+          onClick={() => setShowMenu(false)}
         />
-      )} */}
-
+      )}
+      <div className=" fixed top-3 left-2 md:hidden z-50 h-fit">
+        <Harmburger showMenu={showMenu} handleMenuToggle={() => setShowMenu(!showMenu)} />
+      </div>
       {/* Sidebar */}
       <nav
         className={`
@@ -48,7 +53,11 @@ const currentMenu = portalMenus[portalType];
         h-full overflow-hidden transition-all duration-300 ease-in-out z-50
         ${isCollapsed ? "w-20" : "w-64"}
         /* Mobile styles */
-        mt-16 transform relative
+        mt-16 transform 
+        ${showMenu ? "translate-x-0" : "-translate-x-full"}
+         md:translate-x-0
+         fixed md:relative
+        
         
         /* Desktop styles */
         md:transform-none md:z-0 
@@ -71,24 +80,24 @@ const currentMenu = portalMenus[portalType];
           </button>
         </div>
         <div
-          className={`hidden md:block w-full text-center p-4 border-b border-tetiary/20 mb-2 transition-all duration-300 px-8
+          className={`w-full text-center p-4 border-b border-tetiary/20 mb-2 transition-all duration-300 px-8
           }`}
         >
-          {/* {!isCollapsed ? ( */}
           <h2 className="text-2xl font-medium uppercase">{portalType} Portal</h2>
-          {/* // ) : ( */}
-          {/* //   <div className="text-xl font-bold">CP</div> */}
-          {/* // )} */}
         </div>
 
         {/* Navigation Menu */}
         <ul className="p-4 space-y-2">
-          {currentMenu.map((item) => (
+          {currentMenu.map((item) => {
+             const isActive = pathname === `/portal/${portalType}/${item.key}`
+            return (
             <li key={item.key}>
               <Link
                 href={`/portal/client/${item.key}`} //`/portal/client/${item.key}`}
                 className={`w-full flex items-center px-4 py-3 text-left rounded-lg transition-all duration-300 ease-in-out
-          hover:bg-tetiary/10 hover:translate-x-1 group relative `}
+          hover:bg-tetiary/10 hover:translate-x-1 group relative
+          ${isActive ? 'bg-tetiary/20 translate-x-1 before:content-[""] before:absolute before:left-0 before:top-1/2 before:-translate-y-1/2 before:w-1 before:h-5 before:bg-secondary before:rounded-r': "bg-transparent translate-x-0"}
+           `}
               >
                 <span
                   className={`w-5 h-5 stroke-current fill-none stroke-2 mr-3 transition-all duration-300 ease-in-out`}
@@ -107,7 +116,7 @@ const currentMenu = portalMenus[portalType];
                 {/* {active === item.key && <span className="absolute top-1/2 left-0 w-1 h-1/3 -translate-y-1/2 transition-all duration-300 ease-in-out bg-secondary rounded-sm"></span>} */}
               </Link>
             </li>
-          ))}
+          )})}
         </ul>
       </nav>
     </>
